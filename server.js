d8d9caa84483c44bf882f73739361e435617e88c
Yujia <u7371774@anu.edu.au>
2024-04-29 17:30:28 +1000

    Install server packages

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("server is working");
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(5100, () => {
+  console.log("server running...");
+});
