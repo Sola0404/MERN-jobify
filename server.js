3bf41fd426af36bc0f34fc39e12073cbba37db41
Yujia <u7371774@anu.edu.au>
2024-04-30 14:54:52 +1000

    Setup express middleware to accept json

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  console.log(req);
+
+  res.json({ message: 'data received', data: req.body })
+});
+
 app.get('/', (req, res) => {
   res.send("hello world");
 });
