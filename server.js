99160c5af57d5387308cc238d31d8b00bb598b2a
Yujia <u7371774@anu.edu.au>
2024-04-30 15:14:24 +1000

    Set up env file and add PORT and NODE_ENV

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import morgan from "morgan";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  // HTTP request logger middleware
+  app.use(morgan('dev'));
+}
 
 app.post('/', (req, res) => {
   console.log(req);
@@ -14,6 +22,8 @@ app.get('/', (req, res) => {
   res.send("hello world");
 });
 
-app.listen(5100, () => {
-  console.log("server running...");
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`server running on Port ${port}`);
 });
