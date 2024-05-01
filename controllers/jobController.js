bcf63a32b48a556429057041b92ae854de28368e
Yujia <u7371774@anu.edu.au>
2024-05-01 17:14:18 +1000

    Update job controller methods to persist data in mongoDB

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -8,6 +8,7 @@ let jobs = [
 ]
 
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs });
 };
 
@@ -18,38 +19,29 @@ export const createJob = async (req, res) => {
 
 export const getJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+  const job = await Job.findById(id);
   if (!job) {
+    // id length should be same
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
   res.status(200).json({ job });
 };
 
 export const updateJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'please provide company and position' });
-  }
-
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true, })
+  if (!updateJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
-
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ msg: 'job modified', job });
+  res.status(200).json({ msg: 'job modified', job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
+  const deletedJob = await Job.findByIdAndDelete(id);
+  if (!deleteJob) {
     return res.status(404).json({ msg: `no job with id ${id}` });
   }
 
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-  res.status(200).json({ msg: 'job deleted' });
+  res.status(200).json({ msg: 'job deleted', job: deletedJob });
 };
