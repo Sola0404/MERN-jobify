7dd159e2f57d7592dc607b6a6310b90ed4c22cfd
Yujia <u7371774@anu.edu.au>
2024-05-02 15:16:00 +1000

    Create a custom not found error

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,6 +1,7 @@
 import "express-async-errors";  // A middleware. we don't need to set "try catch" for every async controller
 import Job from '../models/JobModel.js';
 import { StatusCodes } from "http-status-codes";
+import { NotFoundError } from "../errors/customErrors.js";
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
@@ -15,28 +16,21 @@ export const createJob = async (req, res) => {
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
-  if (!job) {
-    // id length should be same
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
+  if (!job) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ job });
 };
 
 export const updateJob = async (req, res) => {
   const { id } = req.params;
   const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true, })
-  if (!updateJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
+  if (!updateJob) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ msg: 'job modified', job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const deletedJob = await Job.findByIdAndDelete(id);
-  if (!deleteJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
+  if (!deleteJob) throw new NotFoundError(`no job with id ${id}`);
 
   res.status(StatusCodes.OK).json({ msg: 'job deleted', job: deletedJob });
 };
