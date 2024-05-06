9afc3bdeb6809569d6b931401b94e701dbeb314c
Yujia <u7371774@anu.edu.au>
2024-05-06 16:17:03 +1000

    Hash passwords

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,17 @@
 import { StatusCodes } from "http-status-codes";
 import User from '../models/UserModel.js';
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
   // first registered user is an admin
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
+
+  // hash password
+  // salt is a random value added to it before hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
