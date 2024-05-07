334b8007a97b28b71909c643a6b88c0ee11bf598
Yujia <u7371774@anu.edu.au>
2024-05-07 16:29:27 +1000

    Set up proxy, concurrently and axios

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.get('/api/v1/test', (req, res) => {
+  res.json({ msg: 'test route' });
+});
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
