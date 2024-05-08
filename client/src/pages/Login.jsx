86f4587f45c1eba66bed6caeed54d178208ab006
Yujia <u7371774@anu.edu.au>
2024-05-08 16:17:39 +1000

    Remove default value in register and login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,10 @@
-import { Link, Form, redirect, useNavigation } from "react-router-dom";
+import {
+	Link,
+	Form,
+	redirect,
+	useNavigation,
+	useActionData,
+} from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from "../../../utils/customFetch.js";
@@ -7,26 +13,34 @@ import { toast } from "react-toastify";
 export const action = async ({ request }) => {
 	const formData = await request.formData();
 	const data = Object.fromEntries(formData);
+	const errors = { msg: "" };
+	if (data.password.length < 3) {
+		errors.msg = "password too short";
+		return errors;
+	}
 	try {
 		await customFetch.post("/auth/login", data);
 		toast.success("Login successful");
 		return redirect("/dashboard");
 	} catch (error) {
-		toast.error(error?.response?.data?.msg);
-		return error;
+		// toast.error(error?.response?.data?.msg);
+		errors.msg = error.response.data.msg;
+		return errors;
 	}
 };
 
 const Login = () => {
 	const navigation = useNavigation();
 	const isSubmitting = navigation.state === "submitting";
+	const errors = useActionData();
 	return (
 		<Wrapper>
 			<Form method="post" className="form">
 				<Logo />
 				<h4>Login</h4>
-				<FormRow type="email" name="email" defaultValue="john@gmail.com" />
-				<FormRow type="password" name="password" defaultValue="secrect123" />
+				{errors && <p style={{ color: "red" }}>{errors.msg}</p>}
+				<FormRow type="email" name="email" defaultValue="sola@gmail.com" />
+				<FormRow type="password" name="password" defaultValue="sola12345" />
 				<button type="submit" className="btn btn-block" disabled={isSubmitting}>
 					{isSubmitting ? "submitting..." : "submit"}
 				</button>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -25,16 +25,11 @@ const Register = () => {
 			<Form method="post" className="form">
 				<Logo />
 				<h4>Register</h4>
-				<FormRow type="text" name="name" defaultValue="john" />
-				<FormRow
-					type="text"
-					name="lastName"
-					labelText="last name"
-					defaultValue="smith"
-				/>
-				<FormRow type="text" name="location" defaultValue="earth" />
-				<FormRow type="email" name="email" defaultValue="john@gmail.com" />
-				<FormRow type="password" name="password" defaultValue="secret123" />
+				<FormRow type="text" name="name" />
+				<FormRow type="text" name="lastName" labelText="last name" />
+				<FormRow type="text" name="location" />
+				<FormRow type="email" name="email" />
+				<FormRow type="password" name="password" />
 
 				<button type="submit" className="btn btn-block" disabled={isSubmitting}>
 					{isSubmitting ? "submitting..." : "submit"}
