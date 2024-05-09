19d6ba265293a82897adb9d6bf36c4f8ea5689a9
Yujia <u7371774@anu.edu.au>
2024-05-09 11:11:02 +1000

    Implement add job page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,8 @@
 export { default as Logo } from './Logo';
 export { default as FormRow } from './FormRow';
+export { default as FormRowSelect } from './FormRowSelect';
 export { default as Navbar } from './Navbar';
 export { default as SmallSidebar } from './SmallSidebar';
 export { default as BigSidebar } from './BigSidebar';
+
 
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,5 +1,53 @@
+import { FormRow, FormRowSelect } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useOutletContext } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants.js";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
+
 const AddJob = () => {
-    return <h1>AddJob page</h1>;
+	const { user } = useOutletContext();
+	const navigation = useNavigation();
+	const isSubmitting = navigation.state === "submitting";
+
+	return (
+		<Wrapper>
+			<Form method="post" className="form">
+				<h4 className="form-title">add job</h4>
+				<div className="form-center">
+					<FormRow type="text" name="position" />
+					<FormRow type="text" name="company" />
+					<FormRow
+						type="text"
+						labelText="job location"
+						name="jobLocation"
+						defaultValue={user.location}
+					/>
+					<FormRowSelect
+						labelText="job status"
+						name="jobStatus"
+						defaultValue={JOB_STATUS.PENDING}
+						list={Object.values(JOB_STATUS)}
+					/>
+					<FormRowSelect
+						name="jobType"
+						labelText="job type"
+						defaultValue={JOB_TYPE.FULL_TIME}
+						list={Object.values(JOB_TYPE)}
+					/>
+
+					<button
+						type="submit"
+						className="btn btn-block form-btn"
+						disabled={isSubmitting}
+					>
+						{isSubmitting ? "submitting..." : "submit"}
+					</button>
+				</div>
+			</Form>
+		</Wrapper>
+	);
 };
 
 export default AddJob;
