e4af55155fc062fe942550785e97dda3474ac8b8
Yujia <u7371774@anu.edu.au>
2024-05-09 18:00:39 +1000

    Implement edit-job UI and logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,15 @@ import {
 	AllJobs,
 	Profile,
 	Admin,
+	EditJob,
 } from "./pages";
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
 import { action as addJobAction } from "./pages/AddJob";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
+import { loader as editJobLoader } from "./pages/EditJob";
+import { action as editJobAction } from "./pages/EditJob";
 
 export const checkDefaultTheme = () => {
 	const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -69,6 +72,12 @@ const router = createBrowserRouter([
 					{
 						path: "admin",
 						element: <Admin />,
+					},
+					{
+						path: "edit-job/:id",
+						element: <EditJob />,
+						loader: editJobLoader,
+						action: editJobAction,
 					},
 				],
 			},
--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -36,7 +36,9 @@ const Job = ({
 				</div>
 
 				<footer className="actions">
-					<Link className="btn edit-btn">Edit</Link>
+					<Link to={`../edit-job/${_id}`} className="btn edit-btn">
+						Edit
+					</Link>
 					<Form>
 						<button type="submit" className="btn delete-btn">
 							Delete
