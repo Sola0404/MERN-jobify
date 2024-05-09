fecaa6e1a4d181f02dcc489cc8d83b636a637e7b
Yujia <u7371774@anu.edu.au>
2024-05-09 20:18:42 +1000

    Add admin page loader

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,5 +1,28 @@
+import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa";
+
+import { useLoaderData, redirect } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import { toast } from "react-toastify";
+
+export const loader = async () => {
+	try {
+		const response = await customFetch.get("/users/admin/app-stats");
+		return response.data;
+	} catch (error) {
+		toast.error("You are not authorized to view this page");
+		return redirect("/dashboard");
+	}
+};
+
 const Admin = () => {
-    return <h1>Admin page</h1>;
+	const { users, jobs } = useLoaderData();
+
+	return (
+		<Wrapper>
+			<h2>admin page</h2>
+		</Wrapper>
+	);
 };
 
 export default Admin;
