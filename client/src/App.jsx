7ec762f1b7db3700aaad5f877f54bb6f277e13e5
Yujia <u7371774@anu.edu.au>
2024-05-11 15:35:20 +1000

    Add profile action

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import { loader as editJobLoader } from "./pages/EditJob";
 import { action as editJobAction } from "./pages/EditJob";
 import { action as deleteJobAction } from "./pages/DeleteJob";
 import { loader as adminLoader } from "./pages/Admin";
+import { action as profileAction } from "./pages/Profile";
 
 export const checkDefaultTheme = () => {
 	const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -70,6 +71,7 @@ const router = createBrowserRouter([
 					{
 						path: "profile",
 						element: <Profile />,
+						action: profileAction,
 					},
 					{
 						path: "admin",
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,6 +5,26 @@ import { useNavigation, Form } from "react-router-dom";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
 
+export const action = async ({ request }) => {
+	// image files cannot be sent by JSON
+	// we use formData
+	const formData = await request.formData();
+
+	const file = formData.get("avatar");
+	if (file && file.size > 500000) {
+		toast.error("Image size too large");
+		return null;
+	}
+
+	try {
+		await customFetch.patch("/users/update-user", formData);
+		toast.success("Profile updated successfully");
+	} catch (error) {
+		toast.error(error?.response?.data?.msg);
+	}
+	return null;
+};
+
 const Profile = () => {
 	const { user } = useOutletContext();
 	const { name, lastName, email, location } = user;
@@ -18,7 +38,7 @@ const Profile = () => {
 
 				<div className="form-center">
 					<div className="form-row">
-						<label htmlFor="image" className="form-label">
+						<label htmlFor="avatar" className="form-label">
 							Select an image file (max 0.5 MB):
 						</label>
 						<input
