72efe997495708dbd18e2fb1c3766b54e06ec556
Yujia <u7371774@anu.edu.au>
2024-05-11 15:53:08 +1000

    Set multer middleware to handle multipart/form-data

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getApplicationStats, getCurrentUser, updateUser } from "../controllers/userController.js";
 import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
 import { authorizePermissions } from "../middleware/authMiddleware.js";
+import upload from "../middleware/multerMiddleware.js";
 const router = Router();
 
 router.get('/current-user', getCurrentUser);
@@ -9,6 +10,6 @@ router.get('/admin/app-stats', [
   authorizePermissions('admin'),
   getApplicationStats,
 ]);
-router.patch('/update-user', validateUpdateUserInput, updateUser);
+router.patch('/update-user', upload.single('avatar'), validateUpdateUserInput, updateUser);
 
 export default router;
