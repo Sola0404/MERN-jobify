6c53ea667b364142584792da9975703438f36cd0
Yujia <u7371774@anu.edu.au>
2024-05-16 16:38:31 +1000

    Track the search history and submit form programmatically

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -5,28 +5,49 @@ import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from "../../../utils/constants";
 import { useAllJobsContext } from "../pages/AllJobs";
 
 const SearchContainer = () => {
+	const { searchValues } = useAllJobsContext();
+	const { search, jobStatus, jobType, sort } = searchValues;
+
+	const submit = useSubmit();
+
 	return (
 		<Wrapper>
 			<Form className="form">
 				<h5 className="form-title">search form</h5>
 				<div className="form-center">
-					<FormRow type="search" name="search" defaultValue="a" />
+					<FormRow
+						type="search"
+						name="search"
+						defaultValue={search}
+						onChange={(e) => {
+							submit(e.currentTarget.form);
+						}}
+					/>
 					<FormRowSelect
 						labelText="job status"
 						name="jobStatus"
 						list={["all", ...Object.values(JOB_STATUS)]}
-						defaultValue="all"
+						defaultValue={jobStatus}
+						onChange={(e) => {
+							submit(e.currentTarget.form);
+						}}
 					/>
 					<FormRowSelect
 						labelText="job type"
 						name="jobType"
 						list={["all", ...Object.values(JOB_TYPE)]}
-						defaultValue="all"
+						defaultValue={jobType}
+						onChange={(e) => {
+							submit(e.currentTarget.form);
+						}}
 					/>
 					<FormRowSelect
 						name="sort"
-						defaultValue="newest"
+						defaultValue={sort}
 						list={[...Object.values(JOB_SORT_BY)]}
+						onChange={(e) => {
+							submit(e.currentTarget.form);
+						}}
 					/>
 
 					<Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
