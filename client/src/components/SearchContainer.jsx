2fc32f0058d21e46ca7e0acd2b18be53ec0ed506
Yujia <u7371774@anu.edu.au>
2024-05-16 18:14:57 +1000

    Use debounce to add some delay when call the function submit

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,4 +1,4 @@
-import { FormRow, FormRowSelect, SubmitBtn } from ".";
+import { FormRow, FormRowSelect } from ".";
 import Wrapper from "../assets/wrappers/DashboardFormPage";
 import { Form, useSubmit, Link } from "react-router-dom";
 import { JOB_TYPE, JOB_STATUS, JOB_SORT_BY } from "../../../utils/constants";
@@ -10,6 +10,18 @@ const SearchContainer = () => {
 
 	const submit = useSubmit();
 
+	// add some delay to avoid unnecessary processing
+	const debounce = (onChange) => {
+		let timeout;
+		return (e) => {
+			const form = e.currentTarget.form;
+			clearTimeout(timeout);
+			timeout = setTimeout(() => {
+				onChange(form);
+			}, 2000);
+		};
+	};
+
 	return (
 		<Wrapper>
 			<Form className="form">
@@ -19,9 +31,9 @@ const SearchContainer = () => {
 						type="search"
 						name="search"
 						defaultValue={search}
-						onChange={(e) => {
-							submit(e.currentTarget.form);
-						}}
+						onChange={debounce((form) => {
+							submit(form);
+						})}
 					/>
 					<FormRowSelect
 						labelText="job status"
