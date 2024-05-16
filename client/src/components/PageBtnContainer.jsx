f5942ca68ef32c0daac92375659d1cce66f82f43
Yujia <u7371774@anu.edu.au>
2024-05-16 20:54:34 +1000

    Add dots logic to pagination

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -9,12 +9,74 @@ const PageBtnContainer = () => {
 	} = useAllJobsContext();
 	const { search, pathname } = useLocation();
 	const navigate = useNavigate();
-	const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
 
 	const handlePageChange = (pageNumber) => {
 		const searchParams = new URLSearchParams(search);
 		searchParams.set("page", pageNumber);
 		navigate(`${pathname}?${searchParams.toString()}`);
+	};
+
+	const addPageButton = ({ pageNumber, activeClass }) => {
+		return (
+			<button
+				className={`btn page-btn ${activeClass && "active"}`}
+				key={pageNumber}
+				onClick={() => handlePageChange(pageNumber)}
+			>
+				{pageNumber}
+			</button>
+		);
+	};
+
+	const renderPageButtons = () => {
+		const pageButtons = [];
+
+		// Add the first page button
+		pageButtons.push(
+			addPageButton({ pageNumber: 1, activeClass: currentPage === 1 })
+		);
+		// Add dots
+		if (currentPage > 3) {
+			pageButtons.push(
+				<span className="page-btn dots" key="dots-1">
+					....
+				</span>
+			);
+		}
+		// One before current page
+		if (currentPage !== 1 && currentPage !== 2) {
+			pageButtons.push(
+				addPageButton({ pageNumber: currentPage - 1, activeClass: false })
+			);
+		}
+		// Add the current page
+		if (currentPage !== 1 && currentPage !== numOfPages) {
+			pageButtons.push(
+				addPageButton({ pageNumber: currentPage, activeClass: true })
+			);
+		}
+		// One after current page
+		if (currentPage !== numOfPages && currentPage !== numOfPages - 1) {
+			pageButtons.push(
+				addPageButton({ pageNumber: currentPage + 1, activeClass: false })
+			);
+		}
+		if (currentPage < numOfPages - 2) {
+			pageButtons.push(
+				<span className="page-btn dots" key="dots+1">
+					....
+				</span>
+			);
+		}
+		// Add the last page button
+		pageButtons.push(
+			addPageButton({
+				pageNumber: numOfPages,
+				activeClass: currentPage === numOfPages,
+			})
+		);
+
+		return pageButtons;
 	};
 
 	return (
@@ -30,19 +92,7 @@ const PageBtnContainer = () => {
 				<HiChevronDoubleLeft />
 				prev
 			</button>
-			<div className="btn-container">
-				{pages.map((pageNumber) => (
-					<button
-						className={`btn page-btn ${pageNumber === currentPage && "active"}`}
-						key={pageNumber}
-						onClick={() => {
-							handlePageChange(pageNumber);
-						}}
-					>
-						{pageNumber}
-					</button>
-				))}
-			</div>
+			<div className="btn-container">{renderPageButtons()}</div>
 			<button
 				className="btn next-btn"
 				onClick={() => {
