57c5da6a0c333f222dda5cb3efa1ba06b9e1319c
Yujia <u7371774@anu.edu.au>
2024-05-16 14:10:42 +1000

    Set query parameters in get all jobs

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -5,9 +5,48 @@ import mongoose from "mongoose";
 import day from 'dayjs';
 
 export const getAllJobs = async (req, res) => {
-  console.log(req.user);
-  const jobs = await Job.find({ createdBy: req.user.userId });
-  res.status(StatusCodes.OK).json({ jobs });
+  const { search, jobStatus, jobType, sort } = req.query;
+
+  const queryObject = {
+    createdBy: req.user.userId,
+  }
+
+  if (search) {
+    // Case-insensitive regular expression search on position OR company
+    queryObject.$or = [
+      { position: { $regex: search, $options: 'i' } },
+      { company: { $regex: search, $options: 'i' } },
+    ];
+  }
+
+  if (jobStatus && jobStatus !== 'all') {
+    queryObject.jobStatus = jobStatus;
+  }
+  if (jobType && jobType !== 'all') {
+    queryObject.jobType = jobType;
+  }
+
+  const sortOptions = {
+    newest: '-createdAt',
+    oldest: 'createdAt',
+    'a-z': 'position',
+    'z-a': '-position',
+  };
+  const sortKey = sortOptions[sort] || sortOptions.newest;
+
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const jobs = await Job.find(queryObject)
+    .sort(sortKey)
+    .skip(skip)
+    .limit(limit);
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ totalJobs, numOfPages, currentPage: page, jobs });
 };
 
 export const createJob = async (req, res) => {
