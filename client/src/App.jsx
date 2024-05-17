42f3f09bb0194bd759312b53b3e3360d7018ad6a
Yujia <u7371774@anu.edu.au>
2024-05-17 15:41:06 +1000

    Create page error element

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,6 +26,7 @@ import { action as deleteJobAction } from "./pages/DeleteJob";
 import { loader as adminLoader } from "./pages/Admin";
 import { action as profileAction } from "./pages/Profile";
 import { loader as statsLoader } from "./pages/Stats";
+import ErrorElement from "./components/ErrorElement";
 
 export const checkDefaultTheme = () => {
 	const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -74,6 +75,7 @@ const router = createBrowserRouter([
 						path: "stats",
 						element: <Stats />,
 						loader: statsLoader,
+						errorElement: <ErrorElement />,
 					},
 					{
 						path: "all-jobs",
