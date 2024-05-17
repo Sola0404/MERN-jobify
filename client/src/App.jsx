469d2f4eed3807ce36cdd9ecbce9265183154e3e
Yujia <u7371774@anu.edu.au>
2024-05-17 16:53:28 +1000

    Set react query for stats

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -74,7 +74,7 @@ const router = createBrowserRouter([
 					{
 						path: "stats",
 						element: <Stats />,
-						loader: statsLoader,
+						loader: statsLoader(queryClient),
 						errorElement: <ErrorElement />,
 					},
 					{
