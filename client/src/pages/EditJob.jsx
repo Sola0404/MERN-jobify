9a32d169ab2f5c740cc364d70ed97a9dd5b20772
Yujia <u7371774@anu.edu.au>
2024-05-19 14:53:23 +1000

    Invalidate jobs

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -15,19 +15,22 @@ export const loader = async ({ params }) => {
 		return redirect("/dashboard/all-jobs");
 	}
 };
-export const action = async ({ request, params }) => {
-	const formData = await request.formData();
-	const data = Object.fromEntries(formData);
+export const action =
+	(queryClient) =>
+	async ({ request, params }) => {
+		const formData = await request.formData();
+		const data = Object.fromEntries(formData);
 
-	try {
-		await customFetch.patch(`/jobs/${params.id}`, data);
-		toast.success("Job edited successfully");
-		return redirect("/dashboard/all-jobs");
-	} catch (error) {
-		toast.error(error?.response?.data?.msg);
-		return error;
-	}
-};
+		try {
+			await customFetch.patch(`/jobs/${params.id}`, data);
+			queryClient.invalidateQueries(["jobs"]);
+			toast.success("Job edited successfully");
+			return redirect("/dashboard/all-jobs");
+		} catch (error) {
+			toast.error(error?.response?.data?.msg);
+			return error;
+		}
+	};
 
 const EditJob = () => {
 	const { job } = useLoaderData();
